beec838d066f0190f41a674334231f75349f5aa2
YanushkaKumar <141440648+YanushkaKumar@users.noreply.github.com>
2025-11-19 20:40:32 +0530

    check poxy

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 // src/services/api.js
 
-const API_BASE_URL = "https://todoappilication.danushka.tech";
+const API_BASE_URL = "http://localhost:5050";
 
 // Helper function to handle API responses
 const handleResponse = async (response) => {
